3930d4e49cf7462b84d92168d1bedaa5d47ad784
AnKub <ankub3088@gmail.com>
2025-04-15 22:03:45 +0300

    fix

--- my-landing/src/App.tsx
+++ my-landing/src/App.tsx
@@ -1,19 +1,19 @@
-import Header from './components/Header';
-import Slider from './components/Slider';
-import Footer from './components/Footer';
-import TextSection from './components/TextSection';
+// import Header from './components/Header';
+// import Slider from './components/Slider';
+// import Footer from './components/Footer';
+// import TextSection from './components/TextSection';
 
-const App = () => {
-  return (
-    <div className="wrapper">
-      <Header />
-      <main className="page">
-        <Slider />
-        <TextSection />
-      </main>
-      <Footer />
-    </div>
-  );
-};
+// const App = () => {
+//   return (
+//     <div className="wrapper">
+//       <Header />
+//       <main className="page">
+//         <Slider />
+//         <TextSection />
+//       </main>
+//       <Footer />
+//     </div>
+//   );
+// };
 
-export default App;
+// export default App;
