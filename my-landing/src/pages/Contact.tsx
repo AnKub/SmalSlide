c28babe8d578c37b18608957e01b74489e1b2c23
AnKub <ankub3088@gmail.com>
2025-04-21 22:06:07 +0300

    components

--- my-landing/src/pages/Contact.tsx
+++ my-landing/src/pages/Contact.tsx
@@ -1,11 +1,11 @@
 
 
-// const Contakt = () => {
-//   return (
-//     <>
-//     <h2>Yololo</h2>
-//     </>
-//   );
-// };
+const Contakt = () => {
+  return (
+    <>
+    <h2>Yolo</h2>
+    </>
+  );
+};
 
-// export default Contakt;
+export default Contakt;
