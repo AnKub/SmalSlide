dbed73b7a7a2e5a0684f06a8546801876614a96a
AnKub <ankub3088@gmail.com>
2025-04-29 12:50:19 +0300

    work and fixing routs

--- my-landing/src/components/Slider.tsx
+++ my-landing/src/components/Slider.tsx
@@ -1,39 +1,39 @@
-// import useEmblaCarousel from 'embla-carousel-react';
-// import Autoplay from 'embla-carousel-autoplay';
-// import '../styles/Slider.scss';
-// import { useRef } from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
+import Autoplay from 'embla-carousel-autoplay';
+import '../styles/Slider.scss';
+import { useRef } from 'react';
 
-// const images = [
-//   { src: '/img/01.jfif', title: 'Some foto of somthing', alt: 'image' },
-//   { src: '/img/02.jfif', title: 'Some fo', alt: 'image' },
-//   { src: '/img/03.jfif', title: 'Some foto of', alt: 'image' },
-//   { src: '/img/04.jfif', title: 'Som', alt: 'image' },
-//   { src: '/img/05.jfif', title: 'Something', alt: 'image' },
-//   { src: '/img/06.jfif', title: 'Something', alt: 'image' },
-//   { src: '/img/07.jfif', title: 'Something', alt: 'image' },
-// ];
+const images = [
+  { src: '/img/01.jfif', title: 'Some foto of somthing', alt: 'image' },
+  { src: '/img/02.jfif', title: 'Some fo', alt: 'image' },
+  { src: '/img/03.jfif', title: 'Some foto of', alt: 'image' },
+  { src: '/img/04.jfif', title: 'Som', alt: 'image' },
+  { src: '/img/05.jfif', title: 'Something', alt: 'image' },
+  { src: '/img/06.jfif', title: 'Something', alt: 'image' },
+  { src: '/img/07.jfif', title: 'Something', alt: 'image' },
+];
 
-// const Slider = () => {
-//   const autoplay = useRef(
-//     Autoplay({ delay: 3000, stopOnInteraction: false }) 
-//   );
+const Slider = () => {
+  const autoplay = useRef(
+    Autoplay({ delay: 3000, stopOnInteraction: false }) 
+  );
 
-//   const [emblaRef] = useEmblaCarousel(
-//     { loop: true, align: 'center' },
-//     [autoplay.current]
-//   );
+  const [emblaRef] = useEmblaCarousel(
+    { loop: true, align: 'center' },
+    [autoplay.current]
+  );
 
-//   return (
-//     <div className="slider" ref={emblaRef}>
-//       <div className="slider__container">
-//         {images.map((img, index) => (
-//           <div className="slider__slide" key={index}>
-//             <img src={img.src} alt={img.alt} title={img.title} />
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div className="slider" ref={emblaRef}>
+      <div className="slider__container">
+        {images.map((img, index) => (
+          <div className="slider__slide" key={index}>
+            <img src={img.src} alt={img.alt} title={img.title} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-// export default Slider;
+export default Slider;
