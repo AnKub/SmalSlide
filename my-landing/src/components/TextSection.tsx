77e4685748e05ec9ce63f45040d1a5e6ed6de0c9
AnKub <ankub3088@gmail.com>
2025-05-03 13:39:45 +0300

    readme

--- my-landing/src/components/TextSection.tsx
+++ my-landing/src/components/TextSection.tsx
@@ -1,33 +1,33 @@
-import { useEffect, useRef, useState } from 'react';
-import '../styles/TextSection.scss';
+// import { useEffect, useRef, useState } from 'react';
+// import '../styles/TextSection.scss';
 
-const TextSection = () => {
-  const sectionRef = useRef<HTMLDivElement>(null);
-  const [isVisible, setIsVisible] = useState(false);
+// const TextSection = () => {
+//   const sectionRef = useRef<HTMLDivElement>(null);
+//   const [isVisible, setIsVisible] = useState(false);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => setIsVisible(entry.isIntersecting),
-      { threshold: 0.3 }
-    );
-    if (sectionRef.current) observer.observe(sectionRef.current);
-    return () => observer.disconnect();
-  }, []);
+//   useEffect(() => {
+//     const observer = new IntersectionObserver(
+//       ([entry]) => setIsVisible(entry.isIntersecting),
+//       { threshold: 0.3 }
+//     );
+//     if (sectionRef.current) observer.observe(sectionRef.current);
+//     return () => observer.disconnect();
+//   }, []);
 
-  return (
-    <div className="wr" ref={sectionRef}>
-      <div className={`text-section ${isVisible ? 'visible' : 'hidden'}`}>
-        <div className="text-section__wr">
-          <p className="text-section__value">
-            <span>"The most merciful thing in the world..."</span><br />
-            <span>"The human beings..."</span><br /><br />
-            <span>"The seas that lap..."</span>
-          </p>
-        </div>
-        <div className="text-section__mask"></div>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className="wr" ref={sectionRef}>
+//       <div className={`text-section ${isVisible ? 'visible' : 'hidden'}`}>
+//         <div className="text-section__wr">
+//           <p className="text-section__value">
+//             <span>"The most merciful thing in the world..."</span><br />
+//             <span>"The human beings..."</span><br /><br />
+//             <span>"The seas that lap..."</span>
+//           </p>
+//         </div>
+//         <div className="text-section__mask"></div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default TextSection;
+// export default TextSection;
