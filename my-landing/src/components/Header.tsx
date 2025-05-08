148398aa3a6d3b905b82856322b8d5f3f4a6616a
AnKub <ankub3088@gmail.com>
2025-05-08 09:28:37 +0300

    text-section

--- my-landing/src/components/Header.tsx
+++ my-landing/src/components/Header.tsx
@@ -1,31 +1,31 @@
-import { Link } from 'react-router-dom';
-import MiniWeather from "./MiniWeather";
-import '../styles/Header.scss'; 
+// import { Link } from 'react-router-dom';
+// import MiniWeather from "./MiniWeather";
+// import '../styles/Header.scss'; 
 
-const Header = () => {
-  return (
-    <header className="header">
-  <nav className="header__nav">
-  <Link to="/" className="header__logo">Nice</Link>
+// const Header = () => {
+//   return (
+//     <header className="header">
+//   <nav className="header__nav">
+//   <Link to="/" className="header__logo">Nice</Link>
 
-  <div className="header__links">
-    <Link to="/">Home</Link>
-    <Link to="/blog">Blog</Link>
-    <Link to="/contact">Contact</Link>
-    <Link to="/weather">Weather</Link>
-  </div>
+//   <div className="header__links">
+//     <Link to="/">Home</Link>
+//     <Link to="/blog">Blog</Link>
+//     <Link to="/contact">Contact</Link>
+//     <Link to="/weather">Weather</Link>
+//   </div>
 
     
-  <div className="header__extras">
-    <div className="header__weather">
-          <MiniWeather />
-        </div>
-    <Link to="/login" className="header__login-button">Login</Link>
-  </div>
-</nav>
+//   <div className="header__extras">
+//     <div className="header__weather">
+//           <MiniWeather />
+//         </div>
+//     <Link to="/login" className="header__login-button">Login</Link>
+//   </div>
+// </nav>
 
-    </header>
-  );
-};
+//     </header>
+//   );
+// };
 
-export default Header;
+// export default Header;
