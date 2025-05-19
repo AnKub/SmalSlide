5d7c24c725bd4896117a3debb9691c1512335a93
AnKub <ankub3088@gmail.com>
2025-05-19 09:52:51 +0300

    make library page

--- my-landing/src/pages/Study.tsx
+++ my-landing/src/pages/Study.tsx
@@ -1,35 +1,35 @@
-// import { useEffect, useState } from "react";
-// import '../styles/Library.scss';
+// // import { useEffect, useState } from "react";
+// // import '../styles/Library.scss';
 
-// interface ArtObject{
-// objectID: number;
-// title: string;
-// primaryImageSmall:string;
-// artistDisplayName:string;
-// objectDate: string;
-// medium: string;
-// culture: string;
-// department:string;
-// dimensions: string;
+// // interface ArtObject{
+// // objectID: number;
+// // title: string;
+// // primaryImageSmall:string;
+// // artistDisplayName:string;
+// // objectDate: string;
+// // medium: string;
+// // culture: string;
+// // department:string;
+// // dimensions: string;
+// // }
+
+// // const CATEGORIES = [
+// //   'Architecture',
+// //   'Sculpture',
+// //   'Paintings',
+// //   'Drawings',
+// //   'Photographs',
+// //   'Furniture',
+// // ]
+// // const formatCategoryForQuery = (category:string) => category.toLowerCase();
+// // const [artworks, setArtworks] = useState<ArtObject[]>([]);
+// // const [menuOpen, setMenuOpen] = useState(false);
+// // const [selectedCategory, setSelectedCategry] = useState('Architecture');
+// // const [loading, setLoading] = useState(false);
+// // const [error, setError] = useState(false);
+
+// // const fetchArtworks =async (category:string)=>{
+// //   setLoading(true);
+// //   setError(false);
+// //   setArtworks([])
 // }
-
-// const CATEGORIES = [
-//   'Architecture',
-//   'Sculpture',
-//   'Paintings',
-//   'Drawings',
-//   'Photographs',
-//   'Furniture',
-// ]
-// const formatCategoryForQuery = (category:string) => category.toLowerCase();
-// const [artworks, setArtworks] = useState<ArtObject[]>([]);
-// const [menuOpen, setMenuOpen] = useState(false);
-// const [selectedCategory, setSelectedCategry] = useState('Architecture');
-// const [loading, setLoading] = useState(false);
-// const [error, setError] = useState(false);
-
-// const fetchArtworks =async (category:string)=>{
-//   setLoading(true);
-//   setError(false);
-//   setArtworks([])
-}
