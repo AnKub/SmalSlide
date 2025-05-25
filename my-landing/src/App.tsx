d9e7b1830cf5e29ca55563a41f4826a0c4d276ae
AnKub <ankub3088@gmail.com>
2025-05-25 17:01:53 +0300

    make library page

--- my-landing/src/App.tsx
+++ my-landing/src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import NYTNews from './pages/NYTNews';
-import Contact from './pages/Contact';
-import Login from './pages/Login';
-import Weather from './pages/Weather';
-import Layout from './components/Layout';
-import Library from './pages/Library';
-
-
 import { MantineProvider, createTheme } from "@mantine/core";
+import { Suspense, lazy } from 'react';
+
+const Home = lazy(() => import('./pages/Home'));
+const NYTNews = lazy(() => import('./pages/NYTNews'));
+const Contact = lazy(() => import('./pages/Contact'));
+const Login = lazy(() => import('./pages/Login'));
+const Weather = lazy(() => import('./pages/Weather'));
+const Library = lazy(() => import('./pages/Library'));
+
+
+import Layout from './components/Layout';
 
 const theme = createTheme({});
 
@@ -16,17 +18,18 @@ function App() {
   return (
     <MantineProvider theme={theme}>
       <Router>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/nytnews" element={<NYTNews />} />
-            <Route path="/library" element={<Library />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/weather" element={<Weather />} />
-          </Route>
-
-          <Route path="/login" element={<Login />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/nytnews" element={<NYTNews />} />
+              <Route path="/library" element={<Library />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/weather" element={<Weather />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Suspense>
       </Router>
     </MantineProvider>
   );
