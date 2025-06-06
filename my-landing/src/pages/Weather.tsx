316743f1717d2861d4696f839ed460fba11ffeea
AnKub <ankub3088@gmail.com>
2025-06-06 18:19:54 +0300

    working with api weather

--- my-landing/src/pages/Weather.tsx
+++ my-landing/src/pages/Weather.tsx
@@ -1,61 +1,61 @@
-import React, { useEffect } from "react";
-import { useWeather } from "../Hooks/useWeather";
-import { Container, Button, Card, Text, Image, Loader } from "@mantine/core";
-import styles from "../styles/Weather.module.scss";
+// import React, { useEffect } from "react";
+// import { useWeather } from "../Hooks/useWeather";
+// import { Container, Button, Card, Text, Image, Loader } from "@mantine/core";
+// import styles from "../styles/Weather.module.scss";
 
-const Weather: React.FC = () => {
-  const { weather, error, loading, fetchWeather, loadFromCache } = useWeather();
+// const Weather: React.FC = () => {
+//   const { weather, error, loading, fetchWeather, loadFromCache } = useWeather();
 
-  useEffect(() => {
-    loadFromCache(); 
-  }, []);
+//   useEffect(() => {
+//     loadFromCache(); 
+//   }, []);
 
-  return (
-    <Container className={styles.container}>
-      <div className={styles.inputWrapper}>
-        <input
-            type="text"
-            className={styles.input}
-            placeholder="Enter city"
-        />
-        <Button onClick={fetchWeather} className={styles.button}>
-          Refresh Weather
-        </Button>
+//   return (
+//     <Container className={styles.container}>
+//       <div className={styles.inputWrapper}>
+//         <input
+//             type="text"
+//             className={styles.input}
+//             placeholder="Enter city"
+//         />
+//         <Button onClick={fetchWeather} className={styles.button}>
+//           Refresh Weather
+//         </Button>
 
-        <Text
-          className={`${styles.error} ${error ? styles.visible : ""}`}
-        >
-          {error || ""}
-        </Text>
+//         <Text
+//           className={`${styles.error} ${error ? styles.visible : ""}`}
+//         >
+//           {error || ""}
+//         </Text>
 
-        {loading && <Loader color="blue" className={styles.loader} />}
-      </div>
+//         {loading && <Loader color="blue" className={styles.loader} />}
+//       </div>
 
-      {weather && (
-        <Card className={styles.card}>
-          <Text className={styles.cityName}>{weather.name}</Text>
+//       {weather && (
+//         <Card className={styles.card}>
+//           <Text className={styles.cityName}>{weather.name}</Text>
 
-          <div className={styles.weatherDetails}>
-            <div className={styles.leftSide}>
-              <Image
-                src={`https://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
-                alt="weather icon"
-                className={styles.weatherIcon}
-              />
-              <Text className={styles.weatherDescription}>
-                {weather.weather[0].description}
-              </Text>
-            </div>
-            <div className={styles.rightSide}>
-              <Text className={styles.temperature}>
-                {Math.round(weather.main.temp)}°C
-              </Text>
-            </div>
-          </div>
-        </Card>
-      )}
-    </Container>
-  );
-};
+//           <div className={styles.weatherDetails}>
+//             <div className={styles.leftSide}>
+//               <Image
+//                 src={`https://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+//                 alt="weather icon"
+//                 className={styles.weatherIcon}
+//               />
+//               <Text className={styles.weatherDescription}>
+//                 {weather.weather[0].description}
+//               </Text>
+//             </div>
+//             <div className={styles.rightSide}>
+//               <Text className={styles.temperature}>
+//                 {Math.round(weather.main.temp)}°C
+//               </Text>
+//             </div>
+//           </div>
+//         </Card>
+//       )}
+//     </Container>
+//   );
+// };
 
-export default Weather;
+// export default Weather;
