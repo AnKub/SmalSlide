1653695f8f06f84f7e4304828f681692fecce10f
AnKub <ankub3088@gmail.com>
2025-06-12 13:37:52 +0300

    make style

--- my-landing/src/components/Header.tsx
+++ my-landing/src/components/Header.tsx
@@ -1,30 +1,53 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import MiniWeather from "./MiniWeather";
-import '../styles/Header.scss'; 
+import defaultAvatar from '/svg/avatar.png'; 
+import '../styles/Header.scss';
 
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    const user = localStorage.getItem('user');
+    if (user) {
+      const parsed = JSON.parse(user);
+      setIsLoggedIn(true);
+      setAvatarUrl(parsed.avatar || null);
+    }
+  }, []);
+
   return (
     <header className="header">
-  <nav className="header__nav">
-  <Link to="/" className="header__logo">Nice</Link>
+      <nav className="header__nav">
+        <Link to="/" className="header__logo">Nice</Link>
 
-  <div className="header__links">
-    <Link to="/">Home</Link>
-    <Link to="/nytnews">NY-Times</Link>
-    <Link to="/philo">Philo</Link>
-    <Link to="/library">Library</Link>
-    <Link to="/weather">Weather</Link>
-  </div>
-
-    
-  <div className="header__extras">
-    <div className="header__weather">
-          <MiniWeather />
+        <div className="header__links">
+          <Link to="/">Home</Link>
+          <Link to="/nytnews">NY-Times</Link>
+          <Link to="/philo">Philo</Link>
+          <Link to="/library">Library</Link>
+          <Link to="/weather">Weather</Link>
         </div>
-    <Link to="/login" className="header__login-button">In</Link>
-  </div>
-</nav>
 
+        <div className="header__extras">
+          <div className="header__weather">
+            <MiniWeather />
+          </div>
+
+          {!isLoggedIn ? (
+            <Link to="/login" className="header__login-button">In</Link>
+          ) : (
+            <Link to="/user" className="header__user-button">
+              <img
+                src={avatarUrl || defaultAvatar}
+                alt="avatar"
+                className="header__user-avatar"
+              />
+            </Link>
+          )}
+        </div>
+      </nav>
     </header>
   );
 };
