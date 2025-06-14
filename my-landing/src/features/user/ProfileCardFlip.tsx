2399d7244770402939a47b27d64b2e5fa70f8046
AnKub <ankub3088@gmail.com>
2025-06-14 10:17:38 +0300

    make user dashboard

--- my-landing/src/features/user/ProfileCardFlip.tsx
+++ my-landing/src/features/user/ProfileCardFlip.tsx
@@ -1,21 +1,21 @@
-// import React from 'react';
-// import '../../styles/ProfileCardFlip.scss';
+import React from 'react';
+import '../../styles/ProfileCardFlip.scss';
 
-// interface ProfileCardFlipProps {
-//   flipped: boolean;
-//   front: React.ReactNode;
-//   back: React.ReactNode;
-// }
+interface ProfileCardFlipProps {
+  flipped: boolean;
+  front: React.ReactNode;
+  back: React.ReactNode;
+}
 
-// const ProfileCardFlip: React.FC<ProfileCardFlipProps> = ({ flipped, front, back }) => {
-//   return (
-//     <div className="card-flip-wrapper">
-//       <div className={`card-inner ${flipped ? 'flipped' : ''}`}>
-//         <div className="card-face card-front">{front}</div>
-//         <div className="card-face card-back">{back}</div>
-//       </div>
-//     </div>
-//   );
-// };
+const ProfileCardFlip: React.FC<ProfileCardFlipProps> = ({ flipped, front, back }) => {
+  return (
+    <div className="card-flip-wrapper">
+      <div className={`card-inner ${flipped ? 'flipped' : ''}`}>
+        <div className="card-face card-front">{front}</div>
+        <div className="card-face card-back">{back}</div>
+      </div>
+    </div>
+  );
+};
 
-// export default ProfileCardFlip;
+export default ProfileCardFlip;
