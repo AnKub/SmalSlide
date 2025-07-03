bbb12c78a5bdb8798b05665910aec810ad0389b6
AnKub <ankub3088@gmail.com>
2025-07-03 15:29:15 +0300

    make login route

---
+++ backend-next/app/api/auth/login/route.ts
@@ -0,0 +1,7 @@
+import {NextRequest, NextResponse} from 'next/server';
+import { prisma } from '../../../../lib/prisma';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = process.env.JWT_SECRET || 'dev_secret_key'
+
