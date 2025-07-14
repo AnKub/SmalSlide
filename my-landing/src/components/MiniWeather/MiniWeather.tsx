72fa6ca3a583dce31d8b59edf7e790481b5a1220
AnKub <ankub3088@gmail.com>
2025-07-14 13:12:51 +0300

    fix weather fetch

--- my-landing/src/components/MiniWeather/MiniWeather.tsx
+++ my-landing/src/components/MiniWeather/MiniWeather.tsx
@@ -1,51 +1,51 @@
-import React, { useEffect } from 'react';
-import { useWeather } from '../../Hooks/useWeather';
-import { Image, Text } from '@mantine/core';
-import { showNotification } from '@mantine/notifications';
-import { IconAlertCircle } from '@tabler/icons-react';
-import styles from './MiniWeather.module.scss';
+// import React, { useEffect } from 'react';
+// import { useWeather } from '../../Hooks/useWeather';
+// import { Image, Text } from '@mantine/core';
+// import { showNotification } from '@mantine/notifications';
+// import { IconAlertCircle } from '@tabler/icons-react';
+// import styles from './MiniWeather.module.scss';
 
-const MiniWeather: React.FC = () => {
-  const { weather, fetchWeather, setError, error, loadFromCache } = useWeather();
+// const MiniWeather: React.FC = () => {
+//   const { weather, fetchWeather, setError, error, loadFromCache } = useWeather();
 
-  useEffect(() => {
-    loadFromCache();
-    fetchWeather();
-  }, []);
+//   useEffect(() => {
+//     loadFromCache();
+//     fetchWeather();
+//   }, []);
 
-  useEffect(() => {
-    if (error) {
-      showNotification({
-        title: 'Weather Error',
-        message: error,
-        color: 'red',
-        icon: <IconAlertCircle size="1rem" />,
-      });
+//   useEffect(() => {
+//     if (error) {
+//       showNotification({
+//         title: 'Weather Error',
+//         message: error,
+//         color: 'red',
+//         icon: <IconAlertCircle size="1rem" />,
+//       });
 
-      const timeout = setTimeout(() => setError(''), 5000);
-      return () => clearTimeout(timeout);
-    }
-  }, [error, setError]);
+//       const timeout = setTimeout(() => setError(''), 5000);
+//       return () => clearTimeout(timeout);
+//     }
+//   }, [error, setError]);
 
-  return (
-    <div className={styles.miniWeather}>
-      {weather ? (
-        <>
-          <Image
-            src={`https://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
-            alt="weather icon"
-            className={styles.icon}
-          />
-          <Text className={styles.temp}>{Math.round(weather.main.temp)}°C</Text>
-        </>
-      ) : (
-        <>
-          <div className={styles.iconPlaceholder} />
-          <Text className={styles.tempPlaceholder}>--°C</Text>
-        </>
-      )}
-    </div>
-  );
-};
+//   return (
+//     <div className={styles.miniWeather}>
+//       {weather ? (
+//         <>
+//           <Image
+//             src={`https://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+//             alt="weather icon"
+//             className={styles.icon}
+//           />
+//           <Text className={styles.temp}>{Math.round(weather.main.temp)}°C</Text>
+//         </>
+//       ) : (
+//         <>
+//           <div className={styles.iconPlaceholder} />
+//           <Text className={styles.tempPlaceholder}>--°C</Text>
+//         </>
+//       )}
+//     </div>
+//   );
+// };
 
-export default MiniWeather;
+// export default MiniWeather;
